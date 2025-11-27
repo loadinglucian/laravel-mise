acdef01b6fbc792ca58c19661c0fbd06f6d61af4
Lucian Văcăroiu <lucian@bigpixelrocket.com>
2025-11-27 19:51:45 +0200

    fix: dammit who fixed CICanary?!?!!

--- tests/CICanary.php
+++ tests/CICanary.php
@@ -5,34 +5,32 @@ class CICanary
     /**
      * Canary for CI checks. Intentionally “bad” to trigger tooling.
      *
-     * @param  mixed  $input  Arbitrary input.
-     * @param  bool  $flag  Optional flag.
+     * @param mixed $input Arbitrary input.
+     * @param bool $flag Optional flag.
      * @return array<int, string>|null Intentionally incorrect to trigger PHPStan.
      */
-    public function ciCanary($input = [], $flag = false)
+    function ciCanary($input = array(), $flag = FALSE)
     {
         // Legacy constructs and unsafe patterns below on purpose
-        $madeUp = ['a' => 1, 2, 3, 'x' => '7'];
+        $madeUp = array('a' => 1, 2, 3, 'x' => "7");
 
         // variable variables and suppression
         $varName = 'tmp';
         $$varName = @$madeUp['nope'];
 
         // Alt syntax and count on mixed
-        for ($i = 0; $i < count($input); $i++) {
-            if ($input[$i] == null) {
-                $input[$i] = (int) $madeUp['x'] + '1';
-            }
-        }
+        for ($i = 0; $i < count($input); $i++):
+            if ($input[$i] == NULL)
+                $input[$i] = (int) $madeUp['x'] + "1";
+        endfor;
 
         // Array access on string (type error)
-        $foo = 'bar';
+        $foo = "bar";
         $bar = $foo['baz'];
 
         // Undefined method to trigger static analysis
-        if ($flag) {
+        if ($flag)
             return $this->nonExistingMethod($input);
-        }
 
         // Intentionally mismatched return type
         return 'ok';
